5be913ead75ed4834a27f775afaad4d2f424cdeb
Asanali12 <sapswung@gmail.com>
2021-09-27 23:32:03 +0300

    changed app.py

--- app_python/app.py
+++ app_python/app.py
@@ -2,9 +2,21 @@
 Main app file
 """
 import requests
+from datetime import datetime
 from flask import Flask
 
 app = Flask(__name__)
+
+
+@app.route('/visits')
+#main time function
+def visits():
+    """
+    Gets vists and returns them
+    """
+    f = open("visits.txt", "r")
+    contents = f.read()
+    return contents
 
 
 @app.route('/')
@@ -13,6 +25,11 @@ def time():
     """
     Gets time data and returns it
     """
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    f = open("visits.txt", "a")
+    f.write("visit at ", current_time)
+    f.close()
     response = requests.get(
         'https://www.timeapi.io/api/Time/current/coordinate?latitude=55.45&longitude=37.36')
     if response.status_code == 200:
